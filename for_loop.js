b18720afc4b629a2e546ee5c3a04423374678e29
naveenkumarkg <navinkumar618@gmail.com>
2022-05-28 10:42:44 +0530

    feat(programs): for loop

---
+++ for_loop.js
@@ -0,0 +1,18 @@
+
+console.log("For Loop");
+
+//  Program to display text 5 times
+// Print odd or even 
+// for(var i=1; i<=100;i++){
+//     if(i%2 === 0){
+//         console.log("I Love Javascript" + i);
+//     }
+
+// }
+
+// Sum of Natural numbers 
+result =0;
+for(let i =0;i<=5;i++){
+
+}
+
